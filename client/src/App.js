f6d16fca0c0ba9342cf48721961e0ac1c5a216aa
sdbowen <sdbowen.git@gmail.com>
2018-09-15 17:34:34 -0500

    Install react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 
@@ -7,10 +8,12 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
