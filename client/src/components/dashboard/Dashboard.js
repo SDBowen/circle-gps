29fabfbcc3ae53ce75a5fb9f897f5f0e3e5aa4e5
sdbowen <sdbowen.git@gmail.com>
2018-09-22 22:21:08 -0500

    Add profile state to dashboard and check for empty profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -9,15 +9,39 @@ class Dashboard extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <div>Here is the dash</div>
-      </div>
-    );
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <h4>Loading...</h4>;
+    } else {
+      if (Object.keys(profile).length > 0) {
+        // Display user profile
+        dashboardContent = <h4>Profile will display here</h4>;
+      } else {
+        // No profile created
+        dashboardContent = <h4>Create a profile</h4>;
+      }
+    }
+
+    return <div>{dashboardContent}</div>;
   }
 }
 
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   getCurrentProfile
 )(Dashboard);
