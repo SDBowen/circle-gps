02d0820bb11493b5cf6d39e96478ff481640c4cd
Steven Bowen <sdbowen.git@gmail.com>
2019-01-06 23:12:07 -0700

    Add require path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 const socket = require("socket.io");
 const DeviceManager = require("./services/deviceManager");
 
