f27f3e5e08e46ff317e0096ac53b598a509f96e2
Steven Bowen <sdbowen.git@gmail.com>
2019-01-22 14:48:01 -0700

    Add active class when device is selected

--- client/src/components/dashboard/Device.js
+++ client/src/components/dashboard/Device.js
@@ -2,13 +2,44 @@
 // Review mockup when additonal device support is implemented
 // //////////////////////////////////////////////////////////
 
-import React from "react";
+import React, { Component } from "react";
 
-const Device = props => (
-  <li className="side-nav__item">
-    <a href="#" className="side-nav__link" onClick={props.selectDevice}>
-      <span>{props.profile.deviceName}</span>
-    </a>
-  </li>
-);
+class Device extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeStatus: false
+    };
+  }
+
+  toggleActiveState() {
+    const currentState = this.state.activeStatus;
+
+    this.setState({ activeStatus: !currentState });
+  }
+
+  render() {
+    return (
+      <li
+        className={
+          this.state.activeStatus
+            ? "side-nav__item side-nav__item--active"
+            : "side-nav__item"
+        }
+      >
+        <a
+          href="#"
+          className="side-nav__link"
+          onClick={e => {
+            this.props.selectDevice(e);
+            this.toggleActiveState();
+          }}
+        >
+          <span>{this.props.profile.deviceName}</span>
+        </a>
+      </li>
+    );
+  }
+}
+
 export default Device;
