a11b4849d38a868f00367c17896f80e6c40bf330
Steven Bowen <sdbowen.git@gmail.com>
2019-01-27 22:20:46 -0700

    Add actions and reducers

--- client/src/reducers/socketReducer.js
+++ client/src/reducers/socketReducer.js
@@ -1,11 +1,34 @@
-import { UPDATE_COORDS } from "../actions/types";
+import {
+  UPDATE_COORDS,
+  ADD_DEVICE,
+  REMOVE_DEVICE,
+  STATE_RESET
+} from "../actions/types";
 
 const initialState = {
-  lastCoords: { lat: 0, lon: 0 }
+  lastCoords: { lat: 0, lon: 0 },
+  addDevice: null,
+  removeDevice: null
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case ADD_DEVICE:
+      return {
+        ...state,
+        addDevice: action.payload
+      };
+    case REMOVE_DEVICE:
+      return {
+        ...state,
+        removeDevice: action.payload
+      };
+    case STATE_RESET:
+      return {
+        ...state,
+        removeDevice: null,
+        addDevice: null
+      };
     case UPDATE_COORDS:
       return {
         ...state,
