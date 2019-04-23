a2636df2f3e5e6528c3e4ad7010b5d05489737c5
sdbowen <sdbowen.git@gmail.com>
2019-04-23 11:57:14 -0600

    Move Map component logic to Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { logoutUser } from '../../actions/authActions';
-import { addUser, addDevice, removeDevice } from '../../actions/socketActions';
+import { addUser, addDevice, removeDevice, stateReset } from '../../actions/socketActions';
 import { clearCurrentProfile } from '../../actions/profileActions';
 
 import Navbar from '../layout/Navbar';
@@ -15,7 +15,8 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      devices: {}
+      devices: {},
+      deviceAction: {}
     };
   }
 
@@ -30,6 +31,23 @@ class Dashboard extends Component {
     addUser(auth.user.id);
   }
 
+  componentDidUpdate(prevProps) {
+    if (
+      // Check socket data for change
+      JSON.stringify(this.props.socket) !== JSON.stringify(prevProps.socket)
+    ) {
+      if (this.props.socket.lastCoords) {
+        const data = {};
+
+        data.deviceId = this.props.socket.lastCoords.id;
+        data.lat = this.props.socket.lastCoords.lat;
+        data.lon = this.props.socket.lastCoords.lon;
+
+        this.updateDeviceCoordinate(data);
+      }
+    }
+  }
+
   onLogoutClick = event => {
     const { clearCurrentProfile, logoutUser } = this.props;
     event.preventDefault();
@@ -41,10 +59,14 @@ class Dashboard extends Component {
   selectDevice = deviceId => {
     const { addDevice, removeDevice, auth } = this.props;
     const { devices } = { ...this.state };
+    const action = {};
 
     devices[deviceId].active = !devices[deviceId].active;
+    action.type = 'status';
+    action.deviceId = deviceId;
+    action.active = devices[deviceId].active;
 
-    this.setState({ devices });
+    this.setState({ devices, deviceAction: action });
 
     const payload = {};
 
@@ -66,21 +88,36 @@ class Dashboard extends Component {
 
       devices[deviceId] = item;
       devices[deviceId].active = false;
+      devices[deviceId].coords = {};
     });
     this.setState({
       devices
     });
   };
 
+  updateDeviceCoordinate = data => {
+    const { devices } = { ...this.state };
+    const action = data;
+
+    action.type = 'coords';
+    devices[data.deviceId].coords.lat = data.lat;
+    devices[data.deviceId].coords.lon = data.lon;
+
+    this.setState({
+      devices,
+      deviceAction: action
+    });
+  };
+
   render() {
-    const { devices } = this.state;
+    const { devices, deviceAction } = this.state;
 
     return (
       <div>
         <Navbar onLogoutClick={this.onLogoutClick} />
         <div className="content">
           <SideNav devices={devices} selectDevice={this.selectDevice} />
-          <Map />
+          <Map devices={devices} deviceAction={deviceAction} />
         </div>
       </div>
     );
@@ -88,7 +125,8 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
+  socket: state.socket
 });
 
 Dashboard.propTypes = {
@@ -97,10 +135,11 @@ Dashboard.propTypes = {
   removeDevice: PropTypes.func.isRequired,
   clearCurrentProfile: PropTypes.func.isRequired,
   logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.objectOf(PropTypes.object).isRequired
+  auth: PropTypes.objectOf(PropTypes.object).isRequired,
+  stateReset: PropTypes.func.isRequired
 };
 
 export default connect(
   mapStateToProps,
-  { addUser, addDevice, removeDevice, logoutUser, clearCurrentProfile }
+  { addUser, addDevice, removeDevice, logoutUser, clearCurrentProfile, stateReset }
 )(Dashboard);
--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { stateReset } from '../../actions/socketActions';
 
 import '../../../node_modules/leaflet/dist/leaflet.css';
 import L from 'leaflet';
@@ -17,38 +15,27 @@ class Map extends Component {
   }
 
   componentDidMount() {
-    // set up the map
-    let map;
-    map = new L.Map('mapid');
-
-    // create the tile layer with attribution
-    var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    var osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a>';
-
-    var osmTileLayer = new L.TileLayer(osmUrl, {
-      attribution: osmAttrib
-    });
-
-    // Set map starting point and tile layer
-    map.setView(new L.LatLng(41.896151, -87.7349909), 12);
-    map.addLayer(osmTileLayer);
+    const map = this.buildMap();
 
     this.setState({ map });
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps, prevState) {
+    const { deviceAction } = this.props;
+    const { mapPins, map } = this.state;
+
     if (
       // Check socket data for change
-      JSON.stringify(this.props.socket) !== JSON.stringify(prevProps.socket)
+      JSON.stringify(deviceAction) !== JSON.stringify(prevProps.deviceAction)
     ) {
-      if (this.props.socket.lastCoords) {
-        const lat = this.props.socket.lastCoords.lat;
-        const lon = this.props.socket.lastCoords.lon;
-        const incomingDeviceId = this.props.socket.lastCoords.id;
+      if (deviceAction.type === 'coords') {
+        const { lat } = deviceAction;
+        const { lon } = deviceAction;
+        const { deviceId } = deviceAction;
 
         // check for existing map pin
-        if (incomingDeviceId in this.state.mapPins) {
-          this.state.mapPins[incomingDeviceId].setLatLng([lat, lon]);
+        if (deviceId in mapPins) {
+          mapPins[deviceId].setLatLng([lat, lon]);
         } else {
           // Create new pin
 
@@ -56,36 +43,54 @@ class Map extends Component {
             color: 'red',
             fillColor: '#f03',
             fillOpacity: 0.5
-          }).addTo(this.state.map);
+          }).addTo(map);
 
-          let mapPins = { ...this.state.mapPins };
-          mapPins[incomingDeviceId] = newDeviceMarker;
+          const pins = { ...mapPins };
+          pins[deviceId] = newDeviceMarker;
 
-          this.setState({ mapPins });
+          this.setState({ mapPins: pins });
         }
       }
 
-      if (
-        this.props.socket.removeDevice &&
-        this.props.socket.removeDevice !== prevProps.socket.removeDevice
-      ) {
-        const deviceId = this.props.socket.removeDevice;
-        const mapPin = this.state.mapPins[deviceId];
+      if (deviceAction.type === 'status' && !deviceAction.active) {
+        const { deviceId } = deviceAction;
+        const mapPin = mapPins[deviceId];
 
         this.removeMapPin(mapPin, deviceId);
       }
     }
   }
 
+  buildMap = () => {
+    // set up the map
+    const map = new L.Map('mapid');
+
+    // create the tile layer with attribution
+    const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a>';
+
+    const osmTileLayer = new L.TileLayer(osmUrl, {
+      attribution: osmAttrib
+    });
+
+    // Set map starting point and tile layer
+    map.setView(new L.LatLng(41.896151, -87.7349909), 12);
+    map.addLayer(osmTileLayer);
+
+    return map;
+  };
+
   removeMapPin = (pin, deviceId) => {
-    if (this.state.map.hasLayer(pin)) {
-      this.state.map.removeLayer(pin);
+    const { map, mapPins } = this.state;
+
+    if (map.hasLayer(pin)) {
+      map.removeLayer(pin);
     }
 
-    let mapPins = { ...this.state.mapPins };
-    delete mapPins[deviceId];
+    const pins = { ...mapPins };
+    delete pins[deviceId];
 
-    this.setState({ mapPins });
+    this.setState({ mapPins: pins });
   };
 
   render() {
@@ -93,19 +98,8 @@ class Map extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  profile: state.profile,
-  auth: state.auth,
-  socket: state.socket
-});
-
 Map.propTypes = {
-  auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired,
-  stateReset: PropTypes.func.isRequired
+  deviceAction: PropTypes.func.isRequired
 };
 
-export default connect(
-  mapStateToProps,
-  { stateReset }
-)(Map);
+export default Map;
